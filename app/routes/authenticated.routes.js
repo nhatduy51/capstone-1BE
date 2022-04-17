b87c0f72196caae2226e4f4bfd8b24c86ba012df
anaconda875 <hflbtmax@gmail.com>
2022-04-17 14:15:44 +0700

    Update authenticated.routes.js

--- app/routes/authenticated.routes.js
+++ app/routes/authenticated.routes.js
@@ -3,7 +3,7 @@ const db = require("../models");
 const User = db.user;
 const authenticatedRoute = express.Router();
 
-authenticatedRoute.get("/", async (req, res, next) => {
+authenticatedRoute.get("/profile", async (req, res, next) => {
     let user = req.principal;
     if (user) {
         return res.json(await User.findByPk(user.id));
@@ -12,18 +12,21 @@ authenticatedRoute.get("/", async (req, res, next) => {
     }
   })
 
-authenticatedRoute.put("/", async (req, res, next) => {
+authenticatedRoute.put("/profile", async (req, res, next) => {
     let user = req.principal;
     if (!user) {
         return res.status(401).send();
     }
     try {
         let foundUser = await User.findByPk(user.id);
+        if(!foundUser) {
+            return res.status(404).send();
+        }
         foundUser.update({
-            gender: req.body.gender,
-            phoneNumber: req.body.phoneNumber,
-            image: req.body.image,
-            professionalTitle: req.body.professionalTitle
+            gender: req.body.gender ? req.body.gender : null,
+            phoneNumber: req.body.phoneNumber ? req.body.phoneNumber : null,
+            image: req.body.image ? req.body.image : null,
+            professionalTitle: req.body.professionalTitle ? req.body.professionalTitle : null
         });
         return res.json(foundUser);        
     } catch (error) {
